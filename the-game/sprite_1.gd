ffd6f9230464d4d1c21b1b3d13b666a3666dcd73
leleep <kushagrasharma1508@gmail.com>
2026-01-20 01:07:55 +0530

    deleted something

--- the-game/sprite_1.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-func _input(event: InputEvent) -> void:
-	if event is InputEvent and event.is_pressed() == true:
-		if(get_rect()).has_point(to_local(event.position)):
-			print("clicked")
